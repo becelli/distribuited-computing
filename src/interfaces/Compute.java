7acb332a95cb30b64749d415c63010490e139e1e
becelli <gustavobecelli@gmail.com>
2023-05-17 22:43:09 -0300

    first commit

---
+++ src/interfaces/Compute.java
@@ -0,0 +1,7 @@
+package src.interfaces;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Compute extends Remote {
+  <T> T executeTask(Task<T> task) throws RemoteException;
+}
